7c0b9a880ebf6ba1385ec536d9552aa6eed1f7cb
Tung Vu <vutung.ct@gmail.com>
2019-11-24 21:27:41 +0700

    Allow skipping retry

--- retry/retry.go
+++ retry/retry.go
@@ -1,12 +1,17 @@
 package retry
 
 // Do will do input function for retryTimes if there is error or return if there is no error
-func Do(fn func() error, retryTimes uint32) error {
+func Do(fn func() error, retryTimes uint32, checkErrSkipRetryFuncs ...func(error) bool) error {
 	var err error
 	for i := uint32(0); i < retryTimes; i++ {
 		err = fn()
 		if err == nil {
 			return nil
+		}
+		for _, fn := range checkErrSkipRetryFuncs {
+			if fn(err) {
+				return err
+			}
 		}
 	}
 	return err
